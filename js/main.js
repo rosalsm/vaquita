54bb96449a7a80175df13d5ea63550f3aed441d8
rosa <r215130@gmail.com>
2016-10-03 14:39:19 -0400

    adding some dropdown

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 ;(function(){ //IIFE for angular
 
+  function Dropdown() {
+    document.getElementByClass("dropdownMenu").classList.toggle("show");
+  }
+   $('.rightSide a[href="#menu"]').on('click', function(){
+     $('.dropdown').toggleClass('dropdownMenu show');
+   })
+
   angular.module('vaquita', ['ngRoute'], function($routeProvider){
     $routeProvider
       .when('/',{
@@ -11,8 +18,11 @@
       .when('/specials', {
         templateUrl: 'partials/specials.html'
       })
-      .when('/menu', {
+      .when('/menurestaurant', {
         templateUrl: 'partials/menurestaurant.html'
+      })
+      .when('/menutaqueria', {
+        templateUrl: 'partials/menutaqueria.html'
       })
     })
 
